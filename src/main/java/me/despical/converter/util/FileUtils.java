035fd4a8496ec9515a893131fcb893e683606cc6
Berke Akçen <berkeakcen@gmail.com>
2022-05-15 14:04:39 +0300

    Create runnable jar

--- src/main/java/me/despical/converter/util/FileUtils.java
+++ src/main/java/me/despical/converter/util/FileUtils.java
@@ -1,6 +1,5 @@
 package me.despical.converter.util;
 
-import com.sun.istack.internal.NotNull;
 import javafx.geometry.Insets;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -28,8 +27,9 @@ public class FileUtils {
 	private static final Pattern YT_VIDEO_ID_PATTERN = Pattern.compile(YT_VIDEO_ID_REGEX, Pattern.CASE_INSENSITIVE);
 
 	public static String getVideoId(String url) {
+		final Matcher matcher = YT_VIDEO_ID_PATTERN.matcher(url);
+
 		String id = "";
-		Matcher matcher = YT_VIDEO_ID_PATTERN.matcher(url);
 
 		if (matcher.find()){
 			return matcher.group(1);
@@ -46,7 +46,6 @@ public class FileUtils {
 		return YT_THUMBNAIL_URL.replace("<ID>", getVideoId(videoURL));
 	}
 
-	@NotNull
 	public static String getTitleQuietly(String url) {
 		try {
 			return readJsonFromUrl("https://www.youtube.com/oembed?url=" + getPureVideoURL(url) + "&format=json").getString("title");
@@ -60,19 +59,22 @@ public class FileUtils {
 	}
 
 	private static String readAll(Reader reader) throws IOException {
-		StringBuilder builder = new StringBuilder();
+		final StringBuilder builder = new StringBuilder();
+
 		int bytes;
 
 		while ((bytes = reader.read()) != -1) {
 			builder.append((char) bytes);
 		}
+
 		return builder.toString();
 	}
 
 	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
 		try (InputStream stream = new URL(url).openStream()) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
-			String jsonText = readAll(reader);
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+			final String jsonText = readAll(reader);
+
 			return new JSONObject(jsonText);
 		}
 	}
@@ -82,15 +84,17 @@ public class FileUtils {
 	}
 
 	public static void initImageFromURL(GridPane gridPane, String thumbnailURL, String url, boolean tooltip) {
-		Image image = new Image(thumbnailURL);
-		ImageView imageView = new ImageView();
+		final Image image = new Image(thumbnailURL);
+		final ImageView imageView = new ImageView();
+
 		imageView.setFitWidth(340);
 		imageView.setFitHeight(175);
 		imageView.setImage(image);
 
 		if (tooltip) Tooltip.install(imageView, new Tooltip(url.startsWith("http") ? FileUtils.getTitleQuietly(url) : "Unknown video"));
 
-		HBox borderedImage = new HBox();
+		final HBox borderedImage = new HBox();
+
 		borderedImage.getStyleClass().add("image-border");
 		borderedImage.getChildren().add(imageView);
 
